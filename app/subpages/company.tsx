9b5a42e76de0077907f79229d3a3a2034f3b64ab
arl1275 <54734069+arl1275@users.noreply.github.com>
2024-12-11 22:49:38 -0600

    add some advances on company subpage

--- app/subpages/company.tsx
+++ app/subpages/company.tsx
@@ -1,9 +1,21 @@
 
-import { View } from "react-native"
+import { View, TouchableOpacity, Text } from "react-native"
+import { Company, getCouting } from "../storage/company"
+import { useState } from "react"
+import { styles } from "../../assets/styles/styles"
 
-export const Company = () =>{
-
+export const CompanyPage = () =>{
+    const [companies, setCompanies] = useState<Company[]>()
     return(
-        <View></View>
+        <View>
+            <View style={[styles.rowContainer, { elevation: 0, backgroundColor : 'white' }]}>
+            <TouchableOpacity style={[styles.greenButton]} >
+              <Text style={{ color: "white", fontWeight: "bold", textAlign: "center" }}>CREAR DETALLE</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.pdfButton}>
+              <Text style={{ color: "white", textAlign: "center", fontWeight: "bold" }}>PDF</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
     )
 }
