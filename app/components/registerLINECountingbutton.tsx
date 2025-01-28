00b25c13151eda67de2dc5559af0c9a8f90f4b05
arl1275 <54734069+arl1275@users.noreply.github.com>
2025-01-27 23:14:59 -0600

    funtions on creating line on registers

--- app/components/registerLINECountingbutton.tsx
+++ app/components/registerLINECountingbutton.tsx
@@ -1,21 +1,61 @@
 
 import React, { useState, useEffect } from "react"
-import { View } from "react-native"
+import { Button, TextInput, View } from "react-native"
 import { CoutingDatail } from "../../app/storage/couting";
+import { getFormattedDate } from "../../app/modals/crear_service_detail";
 
-interface props{
-    addCoutingLine : (value : CoutingDatail) => void;
-    DeleteCoutingLine : (value : CoutingDatail) => void;
-    editCoutingLine : (value : CoutingDatail) => void;
-    id_head_Couting : number;
-    id_head_sheet : number;
+interface props {
+    addCoutingLine: (value: CoutingDatail) => void;
+    DeleteCoutingLine: (value: CoutingDatail) => void;
+    editCoutingLine: (value: CoutingDatail) => void;
+    data: CoutingDatail[];
+    id_head_Couting: number;
+    id_head_sheet: number;
 }
 
-export const RegisterCountingLine : React.FC<props> = ({ addCoutingLine, DeleteCoutingLine, editCoutingLine, id_head_Couting, id_head_sheet}) => {
-    const [CoutingLine, setCoutingLine] = useState<CoutingDatail>();
+export const RegisterCountingLine: React.FC<props> = ({ addCoutingLine, DeleteCoutingLine, editCoutingLine, data, id_head_Couting, id_head_sheet }) => {
+    const [CoutingLine, setCoutingLine] = useState<CoutingDatail>({ id : Date.now(),
+        id_service_sheet : null,
+        id_Head_Couting : 0,
+        detail : '',
+        cantidad : 0,
+        descuento : 0,
+        price : 0,
+        _date_ : getFormattedDate()});
+    const [addLine, setAddline] = useState<boolean>(false);
+    const [saveLine, setSaveLine] = useState<boolean>(false);
+    const [valoresFactura, setValoresFactura] = useState<CoutingDatail[]>(data);
+
+    const SaveLineDetail = () =>{
+        updateVAlue('id_service_sheet', id_head_sheet);
+        updateVAlue('id_Head_Couting', id_head_Couting);
+        updateVAlue('_data_', getFormattedDate());
+        addCoutingLine(CoutingLine);
+        isAdding();   
+    }
+
+    useEffect(()=>{setValoresFactura(data)}, [data]);
     
-    return(
+    useEffect(() => {
+        SaveLineDetail();
+    }, [saveLine])
+
+    const isAdding = () => { setAddline(!addLine) };
+    const Add = () =>{ setSaveLine(!saveLine) };
+    const updateVAlue = (value : string, valor : string | number) => { setCoutingLine((prev)=>({... prev, [value] : valor } ))}
+
+    return (
         <View>
+            <Button title="CREAR LINEA" onPress={isAdding} />
+            {addLine &&
+                <View>
+                    <TextInput placeholder="Detalle"/>
+                    <TextInput placeholder="cantidad"/>
+                    <TextInput placeholder="descuento"/>
+                    <TextInput placeholder="precio"/>
+                    <Button title="agregar" onPress={Add}/>
+                </View>
+            }
         </View>
     )
 }
