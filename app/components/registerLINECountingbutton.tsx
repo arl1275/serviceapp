606d6258d329963cb1260b586703d77b89e7ce84
arl1275 <54734069+arl1275@users.noreply.github.com>
2025-01-28 22:52:01 -0600

    funtions on creating line on registers 2

--- app/components/registerLINECountingbutton.tsx
+++ app/components/registerLINECountingbutton.tsx
@@ -1,8 +1,10 @@
 
 import React, { useState, useEffect } from "react"
-import { Button, TextInput, View } from "react-native"
+import { Button, TextInput, View, FlatList, Text } from "react-native"
 import { CoutingDatail } from "../../app/storage/couting";
 import { getFormattedDate } from "../../app/modals/crear_service_detail";
+import { styles } from "../../assets/styles/styles";
+import { Ionicons } from "@expo/vector-icons";
 
 interface props {
     addCoutingLine: (value: CoutingDatail) => void;
@@ -14,47 +16,70 @@ interface props {
 }
 
 export const RegisterCountingLine: React.FC<props> = ({ addCoutingLine, DeleteCoutingLine, editCoutingLine, data, id_head_Couting, id_head_sheet }) => {
-    const [CoutingLine, setCoutingLine] = useState<CoutingDatail>({ id : Date.now(),
-        id_service_sheet : null,
-        id_Head_Couting : 0,
-        detail : '',
-        cantidad : 0,
-        descuento : 0,
-        price : 0,
-        _date_ : getFormattedDate()});
+    const [CoutingLine, setCoutingLine] = useState<CoutingDatail>({
+        id: Date.now(),
+        id_service_sheet: null,
+        id_Head_Couting: 0,
+        detail: '',
+        cantidad: 0,
+        descuento: 0,
+        price: 0,
+        _date_: getFormattedDate()
+    });
     const [addLine, setAddline] = useState<boolean>(false);
     const [saveLine, setSaveLine] = useState<boolean>(false);
     const [valoresFactura, setValoresFactura] = useState<CoutingDatail[]>(data);
 
-    const SaveLineDetail = () =>{
+    const SaveLineDetail = () => {
         updateVAlue('id_service_sheet', id_head_sheet);
         updateVAlue('id_Head_Couting', id_head_Couting);
         updateVAlue('_data_', getFormattedDate());
         addCoutingLine(CoutingLine);
-        isAdding();   
+        isAdding();
     }
 
-    useEffect(()=>{setValoresFactura(data)}, [data]);
-    
+    useEffect(() => { setValoresFactura(data) }, [data]);
+
     useEffect(() => {
         SaveLineDetail();
     }, [saveLine])
 
     const isAdding = () => { setAddline(!addLine) };
-    const Add = () =>{ setSaveLine(!saveLine) };
-    const updateVAlue = (value : string, valor : string | number) => { setCoutingLine((prev)=>({... prev, [value] : valor } ))}
+    const Add = () => { setSaveLine(!saveLine) };
+    const updateVAlue = (value: string, valor: string | number) => { setCoutingLine((prev) => ({ ...prev, [value]: valor })) }
 
     return (
         <View>
             <Button title="CREAR LINEA" onPress={isAdding} />
             {addLine &&
                 <View>
-                    <TextInput placeholder="Detalle"/>
-                    <TextInput placeholder="cantidad"/>
-                    <TextInput placeholder="descuento"/>
-                    <TextInput placeholder="precio"/>
-                    <Button title="agregar" onPress={Add}/>
+                    <TextInput placeholder="Detalle" />
+                    <TextInput placeholder="cantidad" />
+                    <TextInput placeholder="descuento" />
+                    <TextInput placeholder="precio" />
+                    <Button title="agregar" onPress={Add} />
                 </View>
+            }
+            {
+                valoresFactura &&
+                <FlatList
+                    data={valoresFactura}
+                    renderItem={({ item }) => (
+                        <View style={[styles.rowContainer]}>
+                            <Text>{item.detail}</Text>
+                            <Text>{item.cantidad}</Text>
+                            <Text>{item.descuento}</Text>
+                            <Text>{item.price}</Text>
+                            <Ionicons
+                                name='trash'
+                                size={20}
+                                color="red"
+                                style={{ margin: 10 }}
+                                onPress={() => DeleteCoutingLine(item)}
+                            />
+                        </View>
+                    )}
+                />
             }
         </View>
     )
